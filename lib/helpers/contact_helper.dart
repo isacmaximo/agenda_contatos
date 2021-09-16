8e667cdc02ba2d14712a6da064a37a833f558ffb
isacmaximo <isacmaximomoura@gmail.com>
2021-09-16 16:05:26 -0300

    Criando o banco de dados

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,7 +1,10 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'dart:async';
 
 //declarando as variáveis das colunas do banco de dados
 //como as variáveis não vão mudar em nenhum momento, podemos colocar final antes das variáveis
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
@@ -10,6 +13,49 @@ final String imgColumn = "imgColumn";
 
 
 class ContactHelper{
+  //padrão para ter apenas um objeto da classe: singleton
+  //estou criando um objeto dessa própia classe (_instance)
+  //só pode ser chamado dentro dessa classe
+  //em resumo: a classe tem apenas uma instância que pode ser acessada de qualquer local
+  static final ContactHelper _instance = ContactHelper.internal();
+  factory ContactHelper() => _instance;
+  ContactHelper.internal();
+
+  //declarando o banco de dados
+  late Database _db;
+
+  Future<Database> get db async {
+    //se o banco de dado já estiver inicializado:
+    if (_db != null){
+      return _db;
+    }
+    //se o banco de dados não estiver inicializado:
+    else{
+      _db = await initDB();
+      return _db;
+    }
+  }
+
+  //função que vai inicializar o banco de dados
+  Future<Database> initDB() async {
+    //temos que esperar, pois não é instantâneo
+    //aqui é o caminho:
+    final databasesPath = await getDatabasesPath();
+    //caminho que tem o arquivo do banco de dados
+    final path = join(databasesPath, "contacts.db");
+
+    //abrindo o banco de dados:
+    //caminho, versão, função que cria um banco de dados
+    //esta função só é ativada para criar o banco de dados pela primeira vez
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newVersion) async{
+      await db.execute(
+        //comando para criar o banco de dados: nometabela(coluna1 tipo, coluna2 tipo)...
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+
+      );
+    });
+  }
+
 
 }
 
